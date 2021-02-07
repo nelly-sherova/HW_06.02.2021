1d31b32f9457d5b0c7efbee30eca07b1511c4a25
unknown <nellysherova@mail.ru>
2021-02-07 19:11:52 +0300

    4 commit

--- GenericTypesHW/Program.cs
+++ GenericTypesHW/Program.cs
@@ -58,7 +58,7 @@ namespace GenerycTypesHw
                     j++;
                 }
             }
-            if (begin_index > 0 && end_index < 0)
+           /* if (begin_index > 0 && end_index < 0)
             {
                 for (int i = begin_index; i < arr.Length + end_index; i++)
                 {
@@ -66,7 +66,7 @@ namespace GenerycTypesHw
                     newArr[j] = arr[begin_index];
                     j++;
                 }
-            }
+            }*/
             if (begin_index < 0)
             {
                 
@@ -110,7 +110,7 @@ namespace GenerycTypesHw
             Console.WriteLine("Сам массив: ");
             ArrayHelper<string>.ShowArray(arr);
             Console.WriteLine("Метод slice: ");
-            string[] newArr = ArrayHelper<string>.Slice(arr, 0, -3);
+            string[] newArr = ArrayHelper<string>.Slice(arr, 1, -3);
             ArrayHelper<string>.ShowArray(newArr);
         }
     }
